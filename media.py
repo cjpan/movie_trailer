aa116def882663f9a47d06f05a84f1a3c5772db4
Cheng-Jie Pan <chengjie.pan@gmail.com>
2015-08-27 22:26:20 +0800

    Initial commit for P1 of fullstack web developer
    Initial commit for P1 of fullstack web developer

---
+++ media.py
@@ -0,0 +1,15 @@
+class Movie:
+    """Class for movies"""
+    
+    def __init__(self, movie_title, movie_story_line, poster_image, movie_trailer):
+        self.title = movie_title
+        self.story_line = movie_story_line
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = movie_trailer 
+               
+#def main():
+#    pass
+    #open_movies_page(movies)
+
+#if __name__ == "__main__":
+#    main()
